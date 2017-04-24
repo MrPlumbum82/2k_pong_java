abb40356fd2dbe620cfe3050e5d1501b68ccd839
Your Name <sadanmallhi@gmail.com>
2017-04-24 22:23:52 +0500

    Adding Entities, Ball and Paddle Objects

---
+++ java_pong/nbproject/Game/src/Entity/Paddle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author sadan
+ */
+public class Paddle extends MapObject{
+
+	public Paddle(){
+		
+	}	
+
+	public void draw(){
+
+	}
+
+	public void update(){
+
+	}	
+}
